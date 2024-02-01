b4f99358e6530593859ecbfbffa6a38f2999cb68
sangariai01 <sangarisai01@gmail.com>
2024-02-01 13:39:56 +0530

    added pointers files

---
+++ C/pointersprgrm/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  /*  int a = 5; int b;
+    printf("%d \n",a);
+    printf("print the address : %d  %d\n" , &a ,&b);
+    printf("print the size : %d\n" , sizeof(a));
+
+    int *p=&a;
+    printf("value of p                          : %d \t\n", p);
+    printf("address of p                        : %d \t\n", &p);
+    printf("value stored in the address of p    : %d \t\n", *p);
+    printf("-----------\n");*/
+
+
+    char s[10] ="san";
+    printf("%s",s);
+
+    return 0;
+}
+
