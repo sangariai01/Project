993dce076c83f38aa89f67dd042ec52e682432eb
sangariai01 <sangarisai01@gmail.com>
2024-02-04 21:33:53 -0800

    moified pointer prgrm

--- C++/Operatorexamples_cplus/pointersprgrmincplus/main.cpp
+++ C++/Operatorexamples_cplus/pointersprgrmincplus/main.cpp
@@ -11,10 +11,10 @@ int main()
     cout << "--------------------" << endl;
 
     string *a = &project;
-    printf("value of a                          : %p \t\n", a);
-    printf("address of a                        : %p \t\n", a);
-    printf("value stored in the address of a    : %s \t\n", *a);
-    printf("-----------\n");
+    cout << "value of a                          : %p " << a << endl;
+    cout << "address of a                        : %p " << &a << endl;
+    cout << "value stored in the address of a    : %s " << *a << endl;
+    cout << "-----------\n";
 
     float f ;
     cout << "Enter the value " << endl;
@@ -24,16 +24,16 @@ int main()
     cout << "--------------------" << endl;
 
     float *b = &f;
-    printf("value of b                          : %p \t\n", b);
-    printf("address of b                        : %p \t\n", b);
-    printf("value stored in the address of b    : %f \t\n", *b);
-    printf("-----------\n");
+    cout << "value of b                          : %p " << b << endl;
+    cout << "address of b                        : %p " << &b << endl;
+    cout << "value stored in the address of b    : %f " << *b << endl;
+    cout << "--------------------" << endl;
 
     float **c = &b;
-    printf("value of c                          : %p \t\n", c);
-    printf("address of c                        : %p \t\n", c);
-    printf("value stored in the address of c    : %f \t\n", **c);
-    printf("-----------\n");
+    cout << "value of c                          : %p "<<  c << endl;
+    cout << "address of c                        : %p " << &c << endl;
+    cout << "value stored in the address of c    : %f " << **c << endl;
+    cout << "-----------\n";
 
 
     return 0;
