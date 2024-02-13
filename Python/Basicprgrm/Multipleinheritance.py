935521dd25597b84134128f9aa113e5075da41d4
sangariai01 <sangarisai01@gmail.com>
2024-02-12 22:35:47 -0800

    multilevel inheritance file added

---
+++ Python/Basicprgrm/Multipleinheritance.py
@@ -0,0 +1,25 @@
+class google():
+    def __init__(self):
+        print("Welcomr to google family")
+    def chrome(self):
+            print("Hi... Welcome to chrome")
+#g=google()
+#g.chrome()
+            
+class gmail(google):
+    def __init__(self):
+        print("Welcome to gmail")
+        super().chrome()
+    def account(self):
+        print("Log in your gmail account")
+#x=gmail()
+#x.account()
+
+class drive(gmail):
+    def __init__(self):
+        print("Welcome to google drive")
+        super().chrome()
+        super().account()
+y=drive()
+
+         
