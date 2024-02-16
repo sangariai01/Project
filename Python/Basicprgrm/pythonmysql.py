ea5d9484225b8f51033d6622756701f9a156e8d2
sangariai01 <sangarisai01@gmail.com>
2024-02-15 20:58:32 -0800

    modified sql file added

--- Python/Basicprgrm/pythonmysql.py
+++ Python/Basicprgrm/pythonmysql.py
@@ -1,22 +1,22 @@
 import mysql.connector
 con=mysql.connector.connect(
-    host="192.168.1.240",
-    user="AIBATCH01",
-    password="AI@123",
-    database="a"
+    host="localhost",
+    user="root",
+    password="Sangari@2024",
+    database="ai_sangari"
 )
 print(con)
 result=con.cursor()
 result.execute("show tables")
 result_show=result.fetchall()
 for x in result_show:
-    print(x)
-result.execute("select * from sys_info")
+     print(x)
+result.execute("select * from Ai_batch_Stud_list")
 result_show=result.fetchall()
 for x in result_show:
     print(x)
-result.execute("delete from sys_info where sno = 4")
-result.execute("select * from sys_info")
-result_show=result.fetchall()
-for x in result_show:
-    print(x)
+# result.execute("delete from host_summary where sno = 4")
+# result.execute("select * from host_summary")
+# result_show=result.fetchall()
+# for x in result_show:
+#     print(x)
