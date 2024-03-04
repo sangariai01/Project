dd7d0113aec796e13ec524ac693d58e2cc2455ae
sangariai01 <sangarisai01@gmail.com>
2024-03-04 13:34:31 +0530

    padas program added

---
+++ PyBA/demoprgrm.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data={
+    "Sno":[1,2,3],
+    "Names":["San","Jaya","Kayal"]    
+}
+df=pd.DataFrame(data)
+print(df)
+# print(dir(df))
+# for x in dir(df):
+#     print(x)
+
